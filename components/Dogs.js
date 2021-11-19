e04dc6b74feeb9df08a7297c6775ffa297842075
yogajulianp <yogajprasutiyo@gmail.com>
2021-11-19 09:35:51 +0700

    fetch

--- components/Dogs.js
+++ components/Dogs.js
@@ -1,60 +1,111 @@
-import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  FlatList,
+  View,
+  SafeAreaView,
+  Text,
+  Image,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+import axios from "axios";
 
 const Dogs = () => {
-    const [dogs, setDogs] = useState([])
+  const [dogs, setDogs] = useState([]);
 
-    const getDogs = async () => {
-        try {
-            let response = await axios.get('https://dog.ceo/api/breed/hound/images')
-            console.log(response.data);
-        } catch(e) {
-            console.log(e.message);
-        }
+  const API_URL = "https://dog.ceo/api/breed/hound/images/random/10"
+  const getDogs = async () => {
+    try {
+      let response = await axios.get(API_URL);
+      console.log(response.data.message);
+      const data_dogs = await response.data.message;
+      setDogs(data_dogs);
+    } catch (e) {
+      console.log(e.message);
     }
+  };
 
-    useEffect(()=> {
-        getDogs();
-    }, )
+  // const [items, setItems]=useState([]);
 
-    return (
-        <View style={styles.container}>
-            <View style={style.card}>
-                <Image
-                style={styles.feedImage}
-                source={{
-                    uri: 'https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg'
-                }}
-                />
+  // const GetData=()=> {
+  //     axios.get('https://dog.ceo/api/breed/hound/images
+  //     ')
+  //     .then(res=>{
+  //         const data1=(res.data.data)
+  //         console.log('res: ', data1)
+  //         setItems(data1)
+  //     })
+  // }
 
-            </View>
+  useEffect(() => {
+    getDogs();
+  },[]);
 
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}></View>
 
-            <Text>Gallery Dogs</Text>
-            <Text>Gambar</Text>
+      <View style={styles.header}>
+        <Text>Home</Text>
+      </View>
+      {/* <View style={styles.card}>
+        <Image
+          style={styles.feedImage}
+          source={{
+            uri: feed_image,
+          }}
+        />
+      </View> */}
 
-          
-        </View>
-    )
-}
+      <Text>Gallery Dogs</Text>
+      <Text>Gambar</Text>
 
-export default Dogs
+      <FlatList
+        data={dogs}
+        keyExtractor={(item) => `key-${item.index}`}
+        renderItem={({ item }) => {
+            return (
+                <View style={styles.card}>
+                  <Image
+                    style={styles.feedImage}
+                    source={{uri:item}}
+                  />
+                </View>
+              );
+        } }
+      />
+    </View>
+  );
+};
+
+export default Dogs;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ddd',
-    },
-    card: {
-        backgroundColor: '#fff',
-        padding: 10,
-        margin: 10,
-        borderRadius: 10,
-    },
-    feedImage: {
-        height: 300,
-        borderRadius: 8,
-        marginVertical: 15,
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#ddd",
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+  },
+  header: {
+    padding: 20,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  feedImage: {
+    height: 300,
+    width: 300,
+    borderRadius: 8,
+    marginVertical: 10,
+  },
+});
+
+
+
